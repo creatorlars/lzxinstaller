ebe4b6737947d9f93a15372bd39eb80d719cfbb4
Lars Larsen <lars@lzxindustries.net>
2020-11-24 14:37:45 -0800

    edit lzx_en

--- src/lzx_en_US.ts
+++ src/lzx_en_US.ts
@@ -4,9 +4,7 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.ui" line="23"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="80"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="80"/>
         <source>LZX DFU</source>
         <translation type="unfinished"></translation>
     </message>
@@ -14,7 +12,8 @@
         <location filename="mainwindow.ui" line="45"/>
         <location filename="mainwindow.ui" line="140"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="82"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="82"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="96"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="106"/>
         <source>Chromagnon</source>
         <translation type="unfinished"></translation>
     </message>
@@ -22,7 +21,8 @@
         <location filename="mainwindow.ui" line="50"/>
         <location filename="mainwindow.ui" line="145"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="83"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="83"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="97"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="107"/>
         <source>TBC2</source>
         <translation type="unfinished"></translation>
     </message>
@@ -30,7 +30,8 @@
         <location filename="mainwindow.ui" line="55"/>
         <location filename="mainwindow.ui" line="150"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="84"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="84"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="98"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="108"/>
         <source>Memory Palace</source>
         <translation type="unfinished"></translation>
     </message>
@@ -38,7 +39,8 @@
         <location filename="mainwindow.ui" line="60"/>
         <location filename="mainwindow.ui" line="155"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="85"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="85"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="99"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="109"/>
         <source>Diver</source>
         <translation type="unfinished"></translation>
     </message>
@@ -46,44 +48,52 @@
         <location filename="mainwindow.ui" line="65"/>
         <location filename="mainwindow.ui" line="160"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="86"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="86"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="100"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="110"/>
         <source>Escher Sketch</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="79"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="102"/>
         <source>Select Firmware</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="174"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="88"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="88"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="112"/>
         <source>Select Device</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="95"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="89"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="89"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="103"/>
         <source>Detect Hardware</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="23"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="94"/>
+        <source>LZX Firmware Update</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="121"/>
         <location filename="../build/LZX_autogen/include_Debug/ui_mainwindow.h" line="90"/>
-        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="90"/>
+        <location filename="../build/LZX_autogen/include_Release/ui_mainwindow.h" line="104"/>
         <source>Update Firmware</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="37"/>
+        <location filename="mainwindow.cpp" line="41"/>
         <source>dfu-util cannot be found. Either build dfu-util and copy the binary to this directory or symlink it.
 e.g. ln -s /usr/bin/dfu-util %1/.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="158"/>
+        <location filename="mainwindow.cpp" line="178"/>
         <source>Return Code: %1</source>
         <translation type="unfinished"></translation>
     </message>
