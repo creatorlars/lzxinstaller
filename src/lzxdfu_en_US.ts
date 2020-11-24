d019d488fecb30b0c689c5ab50f87313173c83f7
Lars Larsen <lars@lzxindustries.net>
2020-11-24 05:56:36 +0000

    edit cmake

--- src/lzxdfu_en_US.ts
+++ src/lzxdfu_en_US.ts
@@ -5,50 +5,50 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="23"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="88"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="88"/>
         <source>Firmware Update Utility</source>
         <oldsource>Firmware Updater</oldsource>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="45"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="90"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="90"/>
         <source>Chromagnon</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="50"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="91"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="91"/>
         <source>TBC2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="55"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="92"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="92"/>
         <source>Memory Palace</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="60"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="93"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="93"/>
         <source>Diver</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="65"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="94"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="94"/>
         <source>Escher Sketch</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="79"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="96"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="96"/>
         <source>Select Device</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="95"/>
-        <location filename="../build/Example_autogen/include/ui_mainwindow.h" line="97"/>
+        <location filename="../build/LZX_autogen/include/ui_mainwindow.h" line="97"/>
         <source>Detect Hardware</source>
         <translation type="unfinished"></translation>
     </message>
